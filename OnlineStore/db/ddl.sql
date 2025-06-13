c21a9b3e1bbbc19f4609ab95519a90ecf43b1b8e
NakatsuyamaKyo <k-nakatsuyama@mik-s.jp>
2025-06-13 13:51:51 +0900

    #4 指摘対応

--- OnlineStore/db/ddl.sql
+++ OnlineStore/db/ddl.sql
@@ -1,20 +1,20 @@
 -- 購入履歴管理テーブル
-create table OnlineStote(
-Name char(255) not null,
-PurchaseDate DATE not null,
-ProductName char(255) not null,
-Quantity int  not null
+create table purchase_history(
+name char(255) not null,
+purchase_date DATE not null,
+product_name char(255) not null,
+quantity int  not null
 )
 
 -- 在庫管理テーブル
-create table Stock(
-ProductName char(255) not null,
-Quantity int  not null
+create table stock(
+product_name char(255) not null,
+quantity int  not null
 )
 
 -- アカウント管理テーブル
-create table Account(
-Name char(255) not null,
-Password int not null,
-Authority int  not null
+create table account(
+name char(255) not null,
+password int not null,
+authority int  not null
 )
