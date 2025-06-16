a60555ad895bdcbba1ea9646781372077f3e6d77
NakatsuyamaKyo <k-nakatsuyama@mik-s.jp>
2025-06-16 13:18:57 +0900

    #4 指摘対応

--- OnlineStore/db/ddl.sql
+++ OnlineStore/db/ddl.sql
@@ -7,21 +7,22 @@ DROP SCHEMA IF EXISTS onlinestore;
 -- 購入履歴管理テーブル
 create table onlinestore.purchase_history(
 id int not null primary key comment 'ID',
-user_name char(255) not null comment 'ユーザー名',
+user_id Iint  not null comment 'ユーザーID',
 purchase_date DATE not null comment '購入日',
 product_name char(255) not null comment '商品名',
-quantity int  not null comment '数量'
+quantity int  not null comment '数量',
+foreign key (user_id) references onlinestore.account(id)
 ) comment '購入履歴管理テーブル'
 
 -- 在庫管理テーブル
-create table onliestore.stock(
+create table onlinestore.stock(
 id int not null primary key comment 'ID',
 product_name char(255) not null comment '商品名',
 quantity int  not null comment '数量'
 ) comment '在庫管理テーブル'
 
 -- アカウント管理テーブル
-create table onliestore.account(
+create table onlinestore.account(
 id int not null primary key comment 'ID',
 user_name char(255) not null comment 'ユーザー名',
 password int not null comment 'パスワード',
