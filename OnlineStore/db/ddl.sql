885182012946da33cc8e6bc3ed5a9b1a537f2dae
NakatsuyamaKyo <k-nakatsuyama@mik-s.jp>
2025-06-20 11:34:12 +0900

    #4 指摘対応

--- OnlineStore/db/ddl.sql
+++ OnlineStore/db/ddl.sql
@@ -4,14 +4,23 @@ CREATE SCHEMA IF NOT EXISTS onlinestore;
 -- スキーマの削除
 DROP SCHEMA IF EXISTS onlinestore;
 
+-- アカウント管理テーブル
+create table onlinestore.account(
+id int not null primary key comment 'ID',
+user_name char(255) not null comment 'ユーザー名',
+password int not null comment 'パスワード',
+account_type int  not null comment 'アカウントタイプ'
+) comment 'アカウント管理テーブル'
+
 -- 購入履歴管理テーブル
 create table onlinestore.purchase_history(
 id int not null primary key comment 'ID',
-user_id Iint  not null comment 'ユーザーID',
 purchase_date DATE not null comment '購入日',
 product_name char(255) not null comment '商品名',
 quantity int  not null comment '数量',
-foreign key (user_id) references onlinestore.account(id)
+account_id int  not null comment 'アカウントID',
+foreign key (account_id) references onlinestore.account(id)
+  ON DELETE CASCADE
 ) comment '購入履歴管理テーブル'
 
 -- 在庫管理テーブル
@@ -20,11 +29,3 @@ id int not null primary key comment 'ID',
 product_name char(255) not null comment '商品名',
 quantity int  not null comment '数量'
 ) comment '在庫管理テーブル'
-
--- アカウント管理テーブル
-create table onlinestore.account(
-id int not null primary key comment 'ID',
-user_name char(255) not null comment 'ユーザー名',
-password int not null comment 'パスワード',
-account_type int  not null comment 'アカウントタイプ'
-) comment 'アカウント管理テーブル'
