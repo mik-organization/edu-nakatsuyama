bfdc84e40fa6381b093b6d7d137362dfae41d359
NakatsuyamaKyo <k-nakatsuyama@mik-s.jp>
2025-06-20 11:42:34 +0900

    #4 指摘対応

--- OnlineStore/db/ddl.sql
+++ OnlineStore/db/ddl.sql
@@ -1,8 +1,8 @@
--- スキーマの作成
-CREATE SCHEMA IF NOT EXISTS onlinestore;
-
 -- スキーマの削除
 DROP SCHEMA IF EXISTS onlinestore;
+
+-- スキーマの作成
+CREATE SCHEMA IF NOT EXISTS onlinestore;
 
 -- アカウント管理テーブル
 create table onlinestore.account(
@@ -10,7 +10,7 @@ id int not null primary key comment 'ID',
 user_name char(255) not null comment 'ユーザー名',
 password int not null comment 'パスワード',
 account_type int  not null comment 'アカウントタイプ'
-) comment 'アカウント管理テーブル'
+) comment 'アカウント管理テーブル';
 
 -- 購入履歴管理テーブル
 create table onlinestore.purchase_history(
@@ -21,11 +21,11 @@ quantity int  not null comment '数量',
 account_id int  not null comment 'アカウントID',
 foreign key (account_id) references onlinestore.account(id)
   ON DELETE CASCADE
-) comment '購入履歴管理テーブル'
+) comment '購入履歴管理テーブル';
 
 -- 在庫管理テーブル
 create table onlinestore.stock(
 id int not null primary key comment 'ID',
 product_name char(255) not null comment '商品名',
 quantity int  not null comment '数量'
-) comment '在庫管理テーブル'
+) comment '在庫管理テーブル';
