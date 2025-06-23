29dd7cda314ba9a6f64a95bf591f312f12337ae7
NakatsuyamaKyo <k-nakatsuyama@mik-s.jp>
2025-06-23 17:32:53 +0900

    #5 対応途中 mysql接続が失敗

--- OnlineStore/src/main/java/com/example/demo/controller/AccountController.java
+++ OnlineStore/src/main/java/com/example/demo/controller/AccountController.java
@@ -1,9 +1,13 @@
 package com.example.demo.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 import com.example.demo.model.Account;
 import com.example.demo.service.AccountService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/accounts")
@@ -11,15 +15,8 @@ public class AccountController {
 
   @Autowired private AccountService accountService;
 
-  // 登録API（POST）
   @PostMapping
   public Account registerAccount(@RequestBody Account account) {
     return accountService.register(account);
-  }
-
-  // 動作確認用
-  @RequestMapping("/")
-  public String hello() {
-    return "Account API is working!";
   }
 }
--- OnlineStore/src/main/java/com/example/demo/model/Account.java
+++ OnlineStore/src/main/java/com/example/demo/model/Account.java
@@ -1,13 +1,17 @@
 package com.example.demo.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import lombok.Data;
 
+
 @Entity
-@Table(name = "account", schema = "onlinestore")
+@Table(name="account")
 @Data
 public class Account {
-
   @Id
   @Column(name = "id")
   private Integer id;
