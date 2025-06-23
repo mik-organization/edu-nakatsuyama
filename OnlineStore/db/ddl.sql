d6c7707f7bfcfa132303534525da29038a025de9
NakatsuyamaKyo <k-nakatsuyama@mik-s.jp>
2025-06-23 09:18:52 +0900

    テーブル設計（DDL作成） (#11)

---
+++ OnlineStore/db/ddl.sql
@@ -0,0 +1,32 @@
+-- スキーマの削除
+DROP SCHEMA IF EXISTS onlinestore;
+
+-- スキーマの作成
+CREATE SCHEMA IF NOT EXISTS onlinestore;
+
+-- アカウント管理テーブル
+create table onlinestore.account(
+id int not null primary key comment 'ID',
+user_name char(255) not null comment 'ユーザー名',
+password int not null comment 'パスワード',
+account_type int  not null comment 'アカウントタイプ'
+) comment 'アカウント';
+
+-- 購入履歴管理テーブル
+create table onlinestore.purchase_history(
+id int not null primary key comment 'ID',
+purchase_date DATE not null comment '購入日',
+product_name char(255) not null comment '商品名',
+quantity int  not null comment '数量',
+account_id int  not null comment 'アカウントID'
+) comment '購入履歴;
+
+-- 在庫管理テーブル
+create table onlinestore.stock(
+id int not null primary key comment 'ID',
+product_name char(255) not null comment '商品名',
+quantity int  not null comment '数量'
+) comment '在庫';
+
+ALTER TABLE onlinestore.purchase_history ADD FOREIGN KEY (account_id) REFERENCES onlinestore.account(id) ON DELETE CASCADE;
+
