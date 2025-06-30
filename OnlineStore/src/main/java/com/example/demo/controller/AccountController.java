37d2130075aa7b7b0b2802380042e89bf4188055
NakatsuyamaKyo <k-nakatsuyama@mik-s.jp>
2025-06-30 14:12:21 +0900

    #5 指摘対応

--- OnlineStore/src/main/java/com/example/demo/controller/AccountController.java
+++ OnlineStore/src/main/java/com/example/demo/controller/AccountController.java
@@ -1,10 +1,14 @@
 package com.example.demo.controller;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.example.demo.model.Account;
 import com.example.demo.service.AccountService;
@@ -16,7 +20,15 @@ public class AccountController {
   @Autowired private AccountService accountService;
 
   @PostMapping
-  public Account registerAccount(@RequestBody Account account) {
-    return accountService.register(account);
+  public ResponseEntity<Account> registerAccount(@RequestBody Account account) {
+	  Account saved = accountService.register(account);
+
+	  URI location = ServletUriComponentsBuilder
+	    .fromCurrentRequest()
+	    .path("/{id}")
+	    .buildAndExpand(saved.getId())
+	    .toUri();
+
+	  return ResponseEntity.created(location).body(saved);
   }
 }
